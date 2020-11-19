ae405a97a1b921ad04d83ed1567dbe68d741588e
sa77irak <sa77irak@gmail.com>
2020-11-19 11:10:20 +0900

    made counttimetimer function as a callback for setInterval function

--- script.js
+++ script.js
@@ -4,65 +4,63 @@ s- future date
 - timer
 ************************** */
 
-/*********** */
-/*Calculation*/
-/*********** */
+const countDownTimer = () => {
+  // current time in milliseconds
+  let now = Date.now();
+  // target date in milliseconds
+  const NewYear2021_millisecond = new Date(2021, 0, 1, 0, 0, 0).getTime();
+  // remaining time in milliseconds
+  let remainingTime_millisecond = NewYear2021_millisecond - now;
+  // remaining time in seconds
+  remainingTime_second = Math.floor(remainingTime_millisecond / 1000);
 
-// current time in milliseconds
-let now = Date.now();
-// target date
-const NewYear2021 = new Date(2021, 0, 1, 0, 0, 0);
-// target date in milliseconds
-const NewYear2021_millisecond = NewYear2021.getTime();
-// remaining time in milliseconds
-let remainingTime_millisecond = NewYear2021_millisecond - now;
-// remaining time in seconds
-remainingTime_second = Math.floor(remainingTime_millisecond / 1000);
+  // calculating it into month/day/hour/min/sec
+  let month_final = (remainingTime_second / 60 / 60 / 24 / 30)
+    .toString()
+    .split(".")[0];
 
-// calculating it into month/day/hour/min/sec
-let month_final = (remainingTime_second / 60 / 60 / 24 / 30)
-  .toString()
-  .split(".")[0];
+  let day_final = (
+    (remainingTime_second / 60 / 60 / 24 / 30 - month_final) *
+    30
+  )
+    .toString()
+    .split(".")[0];
 
-let day_final = ((remainingTime_second / 60 / 60 / 24 / 30 - month_final) * 30)
-  .toString()
-  .split(".")[0];
+  let hour_final = (
+    ((remainingTime_second / 60 / 60 / 24 / 30 - month_final) * 30 -
+      day_final) *
+    24
+  )
+    .toString()
+    .split(".")[0];
 
-let hour_final = (
-  ((remainingTime_second / 60 / 60 / 24 / 30 - month_final) * 30 - day_final) *
-  24
-)
-  .toString()
-  .split(".")[0];
-
-let minute_final = (
-  (((remainingTime_second / 60 / 60 / 24 / 30 - month_final) * 30 - day_final) *
-    24 -
-    hour_final) *
-  60
-)
-  .toString()
-  .split(".")[0];
-
-let second_final = Math.round(
-  ((((remainingTime_second / 60 / 60 / 24 / 30 - month_final) * 30 -
-    day_final) *
-    24 -
-    hour_final) *
-    60 -
-    minute_final) *
+  let minute_final = (
+    (((remainingTime_second / 60 / 60 / 24 / 30 - month_final) * 30 -
+      day_final) *
+      24 -
+      hour_final) *
     60
-).toString();
-// console.log(month_final, day_final, hour_final, minute_final, second_final);
+  )
+    .toString()
+    .split(".")[0];
 
-/*********** */
-/*Display in UI*/
-/*********** */
+  let second_final = Math.round(
+    ((((remainingTime_second / 60 / 60 / 24 / 30 - month_final) * 30 -
+      day_final) *
+      24 -
+      hour_final) *
+      60 -
+      minute_final) *
+      60
+  ).toString();
+  // console.log(month_final, day_final, hour_final, minute_final, second_final);
 
-// setTimeout(window.location.reload(), 3);
+  // Display in UI
+  document.querySelector(".months h1").textContent = month_final;
+  document.querySelector(".days h1").textContent = day_final;
+  document.querySelector(".hours h1").textContent = hour_final;
+  document.querySelector(".minutes h1").textContent = minute_final;
+  document.querySelector(".seconds h1").textContent = second_final;
+};
 
-document.querySelector(".months h1").textContent = month_final;
-document.querySelector(".days h1").textContent = day_final;
-document.querySelector(".hours h1").textContent = hour_final;
-document.querySelector(".minutes h1").textContent = minute_final;
-document.querySelector(".seconds h1").textContent = second_final;
+window.setInterval(countDownTimer, 500);
